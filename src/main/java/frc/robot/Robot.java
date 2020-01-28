bc12e2b27285e05b4c3f54a4e1d07ddc65528a84
Robotics <robotics@ctcnet.org>
2020-01-28 11:10:50 -0500

    Using OI.java was a better idea all along...

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.OI;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
